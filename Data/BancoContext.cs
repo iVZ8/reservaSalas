566e9fce7cbb2361939d6a40382a24ae1d0bfe29
VZ8 <gahue507@gmail.com>
2025-10-15 13:59:51 -0300

    Adicionado Entidade Usuario

---
+++ Data/BancoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using reservaSalas.Models;
+
+namespace reservaSalas.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace reservaSalas.Models
+{
+    [Table("Usuarios")]
+    [Index(nameof(Email), IsUnique=true)]
+    public class Usuario
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "Formato de e-mail inválido.")]
+        [StringLength(100, ErrorMessage = "O e-mail deve ter até 100 caracteres.")]
+        public string Email { get; set; }
+        [Required]
+        public bool Administrador { get; set; }
+    }
+}
